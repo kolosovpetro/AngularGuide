38025827d1574f0bf4796cb9d93887377988b3d8
Petro <kolosovp94@gmail.com>
2021-01-02 22:39:53 +0100

    updated

--- http-request/src/app/app.component.ts
+++ http-request/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from './user';
+import {Movie} from './movie';
 
 @Component({
     selector: 'app-root',
@@ -13,11 +14,17 @@ export class AppComponent implements OnInit {
     // @ts-ignore
     user: User;
 
+    // @ts-ignore
+    movie: Movie;
+
+    private url = 'https://movies-rest-api-demo.herokuapp.com/api/movies/1';
+
 
     constructor(private http: HttpClient) {
     }
 
     ngOnInit(): void {
-        this.http.get<User>('assets/user.json').subscribe((data: User) => this.user = data);
+        // this.http.get<User>('assets/user.json').subscribe((data: User) => this.user = data);
+        this.http.get<Movie>('assets/user.json').subscribe((data: Movie) => this.movie = data);
     }
 }
---
+++ http-request/src/app/movie.ts
@@ -0,0 +1,16 @@
+export class Movie {
+    // @ts-ignore
+    movieId: number;
+
+    // @ts-ignore
+    title: string;
+
+    // @ts-ignore
+    year: number;
+
+    // @ts-ignore
+    ageRestriction: number;
+
+    // @ts-ignore
+    price: number;
+}
